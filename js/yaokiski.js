6144ced1ac16d10e831613664a6f7337f36d059c
Gustavo Martinez <tavo962@gmail.com>
2016-04-10 09:33:39 -0500

    Changes: share, MainController etc.

--- js/yaokiski.js
+++ js/yaokiski.js
@@ -4,15 +4,107 @@ var yaokiski = angular.module( 'yaokiski', [] )
 
 .config( function() {} )
 
-.factory( 'request', function( $http, url ) {
+.factory( 'display', [ 'url', function( url ) {
+	var display = {};
+
+	display.data = {
+		"control":	null,
+		"device":	{},
+		"screen":	null
+	};
+
+	display.instance = function( data ) {
+		try {
+			url.isValid( data.url );
+			this.setSize( data.size );
+			this.setScreen( data.screen );
+
+			window.open(
+				data.uri,
+				'',
+				'menubar=no, toolbar=no, resizable=no, scrollbars=yes, height=' + this.data.control.height + ',width=' + this.data.control.width + ',left=' + this.data.device.left + ',top=' + this.data.device.top
+			);
+		}	// end try
+
+		catch( error ) {
+			console.error( error.message );
+		}	// end catch
+	};
+
+	display.setScreen = function( screen ) {
+		if( ! screen )
+			throw new Error( 'No data of display.' );
+
+		this.data.screen = screen;
+
+		this.data.device = {
+			"left":	( this.data.screen.width / 2 ) - ( this.data.control.width / 2 ),
+			"top":	( this.data.screen.height / 2 ) - ( this.data.control.height / 2 )
+		};
+	};
+
+	display.setSize = function( size ) {
+		display.data.control = size || {
+			"height":	300,
+			"width":	600
+		};
+	};
+
+	return display;
+} ] )
+
+.factory( 'network', [ 'display', function( display ) {
+	var network = {};
+
+	network.check = function( data ) {
+		if( ! data || ! data.content || ! data.network || ! data.screen || ! data.url )
+			throw new Error( 'Wrong data.' );
+
+		return this.prepare( data );
+	};
+
+	network.prepare = function( data ) {
+		var share = null;
+		
+		switch( data.network ) {
+			case 'facebook':
+				share = 'https://www.facebook.com/sharer/sharer.php?u=' + data.url;
+				break;
+			case 'google-plus':
+				share = 'https://plus.google.com/share?url=' + data.url;
+				break;
+			case 'pinterest':
+				share = 'http://pinterest.com/pin/create/button/?url=' + data.url + '&description=' + data.content + '&media=' + data.media;
+				break;
+			case 'twitter':
+				share = 'https://twitter.com/share?via=CHANGE_USER&text=' + data.content + '&url=' + data.url;
+				break;
+			case 'whatsapp':
+				location.href = encodeURI( 'whatsapp://send?text=CHANGE_USER: ' + data.content + ' ' + data.url );
+				break;
+		}	// end switch
+
+		return encodeURI( share );
+	};
+
+	network.share = function( data ) {
+		try {
+			data.uri = this.check( data );
+			display.instance( data );
+		}	// end try
+
+		catch( error ) {
+			console.error( error.message );
+		}	// end catch
+	};
+
+	return network;
+} ] )
+
+.factory( 'request', [ '$http', 'url', function( $http, url ) {
 	var request = {};
 
 	request.data = null;
-
-	request.isValid = function( url ) {
-		if( ! url )
-			throw new Error( 'Invalid URL in the Request.' );
-	};
 
 	request.youHaveparameters = function( params ) {
 		if( ! params )
@@ -27,7 +119,7 @@ var yaokiski = angular.module( 'yaokiski', [] )
 		var object = null;
 
 		try {
-			this.isValid( url );
+			this.url.isValid( url );
 
 			object = $http( {
 				method:	'GET',
@@ -65,7 +157,7 @@ var yaokiski = angular.module( 'yaokiski', [] )
 		var object = null;
 
 		try {
-			this.isValid( url );
+			this.url.isValid( url );
 			this.youHaveparameters( data );
 
 			object = $http( {
@@ -95,18 +187,24 @@ var yaokiski = angular.module( 'yaokiski', [] )
 		return object;
 	};
 
-	return request;
-} )
+	request.url = url;
 
-// TODO: ver si va en este módulo
+	return request;
+} ] )
+
 .factory( 'url', function() {
 	var url = {};
 
 	url.controller = null;
 
-	// url.setController = function( data ) {
-	// 	this.controller = data;
-	// };
+	url.isValid = function( url ) {
+		if( ! url )
+			throw new Error( 'Invalid URL in the Request.' );
+	};
+
+	url.setController = function( data ) {
+		this.controller = data || {};
+	};
 
 	return url;
 } )
@@ -151,9 +249,7 @@ var yaokiski = angular.module( 'yaokiski', [] )
 	return validation;
 } )
 
-.controller( 'ContactController',[ '$scope', 'request', 'validation', function( $scope, request, validation ) {
-	$scope.validation = validation;
-
+.controller( 'ContactController', [ '$scope', 'request', function( $scope, request ) {
 	$scope.reset = function() {
 		$scope.recaptcha = false;
 		grecaptcha.reset();
@@ -183,5 +279,32 @@ var yaokiski = angular.module( 'yaokiski', [] )
 			}	// end if
 			$scope.reset();
 		} )
+	};
+} ] )
+
+.controller( 'YaokiskiController', [ '$scope', 'request', 'validation', 'network', function( $scope, request, validation, network ) {
+	$scope.validation = validation;
+
+	$scope.go = function( url ) {
+		try {
+			request.url.isValid( url )
+			location.href = url;	
+		}	// end try
+
+		catch( error ) {
+			console.error( error.message );
+		}	// end catch
+	};
+
+	$scope.share = function( event, data, type, url ) {
+		event.preventDefault();
+
+		network.share( {
+			"content":	data.content || null,
+			"media":	data.media || null,
+			"network":	type,
+			"screen":	window.screen,
+			"url":		url
+		} );
 	};
 } ] );
