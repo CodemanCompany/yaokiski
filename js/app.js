51e513daae5849a0661d1be2b0d0347b00c1c9bb
Gustavo Martínez <gustavo@codeman.company>
2018-06-18 21:02:25 -0500

    Progress Linux

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 const app = angular.module( 'app', [ 'ngRoute', 'yaokiski' ] )
 
-.config( [ '$routeProvider', '$locationProvider', ( $routeProvider, $locationProvider ) => {
-	$routeProvider
-	.when( '/', {
-		"templateUrl":	'structure/home.html'
-	} )
-	.when( '/contact', {
-		"templateUrl":	'structure/contact.html'
-	} )
-	.when( '/login', {
-		"templateUrl":	'structure/login.html'
-	} )
-	.otherwise( {
-		"redirectTo":	'/'
-	} );
+// .config( [ '$routeProvider', '$locationProvider', ( $routeProvider, $locationProvider ) => {
+// 	$routeProvider
+// 	.when( '/', {
+// 		"templateUrl":	'structure/home.html'
+// 	} )
+// 	.when( '/contact', {
+// 		"templateUrl":	'structure/contact.html'
+// 	} )
+// 	.when( '/login', {
+// 		"templateUrl":	'structure/login.html'
+// 	} )
+// 	.otherwise( {
+// 		"redirectTo":	'/'
+// 	} );
 
-	$locationProvider.html5Mode( true );
-} ] )
+// 	$locationProvider.html5Mode( true );
+// } ] )
 
-// .config( [ () => {} ] )
+.config( [ () => {} ] )
 
 .controller( 'MainController', [ '$controller', '$scope', ( $controller, $scope ) => {
 	$controller( 'YaokiskiController', { "$scope": $scope } );
